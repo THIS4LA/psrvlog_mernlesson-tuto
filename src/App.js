fcf1e849dfdf2e67485b47af747364e3a0aa6148
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-04-21 01:46:03 +0530

    install tailwind packages

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Thisal Karunarathna's First Successfull Crud After Many Attempts</h1>
+        <h1 className="text-3xl font-bold underline">Welcome to Thisal Karunarathna's First Successfull Crud After Many Attempts</h1>
         <button className='user-button' onClick={() => navigate('/Users')}>Proceed</button>
       </header>
     </div>
