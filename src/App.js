b6f24b2303550453cfb5e14cd93221f861623a50
Ravindu Thisal Karunarathna <111478835+THIS4LA@users.noreply.github.com>
2024-04-21 10:01:26 +0530

    change icons

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 className="text-3xl font-bold underline">Welcome to Thisal Karunarathna's First Successfull Crud After Many Attempts</h1>
-        <button className='user-button' onClick={() => navigate('/Users')}>Proceed</button>
+        <button className='bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-3 border border-gray-400 rounded shadow' onClick={() => navigate('/users')}>Proceed</button>
       </header>
     </div>
   );
